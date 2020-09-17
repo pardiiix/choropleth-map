b262181503e8f85aa8fff4ca98f4e6d6d84cb90b
Pardis Ranjbar <pranjb3@uic.edu>
2020-09-17 12:03:04 +0000

    basic us map

---
+++ script.js
@@ -0,0 +1,43 @@
+//Map dimensions (in pixels)
+var width = 600,
+    height = 350;
+
+//Map projection
+var projection = d3.geoAlbersUsa()
+    .scale(730.2209486090715)
+    .translate([width/2,height/2]) //translate to center the map in view
+
+//Generate paths based on projection
+var path = d3.geoPath() 
+    .projection(projection);
+
+//Create an SVG
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+//Group for the map features
+var features = svg.append("g")
+    .attr("class","features");
+
+
+d3.json("us-states.json").then(function(geodata) {
+
+
+  //Create a path for each map feature in the data
+  features.selectAll("path")
+    .data(geodata.features)
+    .enter()
+    .append("path")
+    .attr("d",path)
+    .on("click",clicked);
+
+});
+
+// Add optional onClick events for features here
+// d.properties contains the attributes (e.g. d.properties.name, d.properties.population)
+function clicked(d,i) {
+
+}
+
+
